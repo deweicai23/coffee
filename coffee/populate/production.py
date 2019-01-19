5c21d04f309d5c37829fdc3cd3bcfe5bedf97648
bill <bill@bill-VirtualBox>
2019-01-19 11:30:42 +0800

    again

--- coffee/populate/production.py
+++ coffee/populate/production.py
@@ -6,4 +6,3 @@ from account.models import User
 print('Creating admin account ... ', end='')
 User.objects.create_superuser(username='admin', password='cdw1314520', email=None, fullName='管理者')
 print('done')
-
